1dd1d7f143915640d0c1bb679de049c5e939d267
babadu1234 <babadu1234@inbox.lv>
2013-07-09 10:36:30 -0400

    Changed up something

--- com/babadu1234/TwitchTVBooster/LoopViews.java
+++ com/babadu1234/TwitchTVBooster/LoopViews.java
@@ -4,11 +4,10 @@ public class LoopViews {
 
 	public static void loopViews() {
 		for (int i = 0; i < Main.numberOfViews; i++) {
-			for (int j = 0; j < i; j++) {
-				TwitchSocket.twitchSocket();
-				System.out.println("Viewing..");
-			}
+			TwitchSocket.twitchSocket();
 		}
+		while (true)
+			;
 	}
 
 }
