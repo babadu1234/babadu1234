2d84bb9ab37f7828c1532b03b249648ca1064c01
babadu1234 <babadu1234@inbox.lv>
2013-07-17 12:40:29 -0400

    Removed.

--- com/babadu1234/ScreenCapture/Main.java
+++
@@ -1,9 +0,0 @@
-package com.babadu1234.ScreenCapture;
-
-public class Main {
-
-	public static void main(String[] args) {
-		CaptureScreen.screenCapture();
-	}
-
-}
--- com/babadu1234/TextFileReader/Main.java
+++
@@ -1,9 +0,0 @@
-package com.babadu1234.TextFileReader;
-
-public class Main {
-
-	public static void main(String[] args) {
-		Reader.textReader();
-	}
-
-}
